1f7d5e871a9fbf0b6ab18ce6ef2902fe18d4562f
mardatilla <mardatilla.annisa96@gmail.com>
2020-01-10 14:08:14 +0700

    untuk mengirimkan data username dan password yang dimasukkan dari user ke sistem.

---
+++ src/helpers/API.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import apiconfig from '../configs/api.configs.json'
+
+const API = {
+    login: async (username, password) => {
+        
+        let option ={
+            url:apiconfig.BASE_URL + apiconfig.ENDPOINTS.LOGIN,
+            method : 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            data:{
+                username:username,
+                password:password
+            }
+        }
+        try {
+            let result = await axios(option)
+    
+            return result.data
+        } catch (error){
+    
+            return error
+        }
+    }
+}
+
+export default API
+    
